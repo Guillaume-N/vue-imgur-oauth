18b5a8698ef3ec656c2f1a38c72c1794d27c3031
Guillaume <noel.guillaume@live.fr>
2019-10-26 10:09:32 +0200

    set Vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App";
+import store from "./store";
 
 new Vue({
+  store, // in addition to telling Vue to use Vuex, we must pass our store to Vue
   render: h => h(App)
 }).$mount("#app");
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import Vuex from "vuex";
+import Vue from "vue";
+
+Vue.use(Vuex); // allows Vuex to talk to Vue
+
+// we export our store
+// we register our modules into it
+export default new Vuex.Store({
+  modules: {}
+});
