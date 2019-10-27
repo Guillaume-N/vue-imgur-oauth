c3328c318e8327f010508314941753f26b21e423
Guillaume <noel.guillaume@live.fr>
2019-10-27 09:52:36 +0100

    syntax

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -4,14 +4,14 @@ const CLIENT_ID = process.env.VUE_APP_IMGUR_CLIENT_ID;
 const ROOT_URL = "https://api.imgur.com";
 
 export default {
-  login: () => {
-    const queryString = {
+  login() {
+    const querystring = {
       client_id: CLIENT_ID,
       response_type: "token"
     };
 
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
-      queryString
+      querystring
     )}`;
   }
 };
