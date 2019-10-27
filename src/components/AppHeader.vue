68687645ad89c79eb458de62bccbfa1e84f8f9ff
Guillaume <noel.guillaume@live.fr>
2019-10-27 09:53:23 +0100

    handling login and logout in AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="ui secondary pointing menu">
     <a href="/" class="active item">Image Storage</a>
-
     <div class="right menu">
-      <a href="#" class="ui item" @click="login">Login</a>
+      <div v-if="isLoggedIn" class="horizontal">
+        <a class="item">Gallery</a>
+        <a class="item">Upload</a>
+        <a class="item" @click="logout">Logout</a>
+      </div>
+      <button v-else class="ui item" @click="login" style="cursor:pointer">Login</button>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "AppHeader",
-  methods: mapActions(["login"])
+  computed: mapGetters(["isLoggedIn"]),
+  methods: mapActions(["login", "logout"])
 };
 </script>
+
+<style scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
+</style>
