e76666b6422c5d735313d830f76b1bea8547e55b
Guillaume <noel.guillaume@live.fr>
2019-10-27 09:52:29 +0100

    added router in main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App";
 import store from "./store";
+import AuthHandler from "./components/AuthHandler";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/oauth2/callback",
+      component: AuthHandler
+    }
+  ]
+});
 
 new Vue({
+  router,
   store, // in addition to telling Vue to use Vuex, we must pass our store to Vue
   render: h => h(App)
 }).$mount("#app");
