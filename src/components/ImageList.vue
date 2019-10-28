3ba78cfa531b7a1b50a34e8d4059c048c1edf547
Guillaume <noel.guillaume@live.fr>
2019-10-28 19:45:01 +0100

    wip - fetching, displaying and uploading images

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>Image List</div>
+  <div>
+    <img v-for="img in allImages" :src="img.link" :key="img.link" alt="img uploaded on imgur.com" />
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
-  name: "ImageList"
+  name: "ImageList",
+  computed: mapGetters(["allImages"]),
+  methods: mapActions(["fetchImages"]),
+  created() {
+    this.fetchImages();
+  }
 };
 </script>
+
+<style scoped>
+img {
+  max-width: 500px;
+  max-height: 500px;
+}
+</style>
